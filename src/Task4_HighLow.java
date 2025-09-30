df57078e0ec42d3831f0f6fa7199c0461032f808
Mason Baldwin <me.baldwin1016@icloud.com>
2025-09-29 21:32:46 -0400

    Task 2

---
+++ src/Task4_HighLow.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task4_HighLow {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        Random rnd = new Random();
+        boolean done = false;
+
+        int numToGuess = rnd.nextInt(10) + 1;// 0-9 + 1 -> 1-10
+        int guess 0;
+
+        String newLine = "";
+        String trash = "";
+
+    }
+}
